08cbf742ed0b7317cace1aa4bcf07b7a6448fafa
Igor Storchevoy <hellsgor@gmail.com>
2024-09-03 13:38:48 +0400

    Add: added images optimization

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,9 @@ import { resolve } from 'path';
 import fg from 'fast-glob';
 import { fileURLToPath } from 'url';
 import path from 'node:path';
+
 import handlebars from 'vite-plugin-handlebars';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 import { context } from './src/stores/context';
 import { home } from './src/stores/home';
@@ -37,6 +39,66 @@ export default defineConfig({
       partialDirectory: resolve(__dirname, './src/components'),
       context(pagePath) {
         return { ...pageData[pagePath], ...context };
+      },
+    }),
+    ViteImageOptimizer({
+      test: /\.(jpe?g|png|gif|tiff|webp|svg|avif)$/i,
+      exclude: undefined,
+      include: undefined,
+      includePublic: true,
+      logStats: true,
+      svg: {
+        multipass: true,
+        plugins: [
+          {
+            name: 'preset-default',
+            params: {
+              overrides: {
+                cleanupNumericValues: false,
+                removeViewBox: false, // https://github.com/svg/svgo/issues/1128
+              },
+              cleanupIDs: {
+                minify: false,
+                remove: false,
+              },
+              convertPathData: false,
+            },
+          },
+          'sortAttrs',
+          {
+            name: 'addAttributesToSVGElement',
+            params: {
+              attributes: [{ xmlns: 'http://www.w3.org/2000/svg' }],
+            },
+          },
+        ],
+      },
+      png: {
+        // https://sharp.pixelplumbing.com/api-output#png
+        quality: 85,
+      },
+      jpeg: {
+        // https://sharp.pixelplumbing.com/api-output#jpeg
+        quality: 85,
+      },
+      jpg: {
+        // https://sharp.pixelplumbing.com/api-output#jpeg
+        quality: 85,
+      },
+      tiff: {
+        // https://sharp.pixelplumbing.com/api-output#tiff
+        quality: 85,
+      },
+      // gif does not support lossless compression
+      // https://sharp.pixelplumbing.com/api-output#gif
+      gif: {},
+      webp: {
+        // https://sharp.pixelplumbing.com/api-output#webp
+        lossless: true,
+      },
+      avif: {
+        // https://sharp.pixelplumbing.com/api-output#avif
+        lossless: true,
       },
     }),
   ],
