96bc112a945233644d7508d6368312edbc6e97c0
Igor Storchevoy <hellsgor@gmail.com>
2024-11-21 16:03:55 +0400

    Update[main]: removed using initialization Forms function

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,7 @@
 import '../../node_modules/modern-normalize/modern-normalize.css';
 import '../scss/style.scss';
-import { initForms } from './services/Form';
 import { setupCounter } from './utils/counter';
 
 window.addEventListener('DOMContentLoaded', () => {
   setupCounter(document.getElementById('counter'));
-
-  initForms();
 });
