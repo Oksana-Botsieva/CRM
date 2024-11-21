ac8c032cde29802808c3a72af7c8a0600482b4b4
Igor Storchevoy <hellsgor@gmail.com>
2024-11-21 15:44:46 +0400

    Feat[Form]: added Form class

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,10 @@
 import '../../node_modules/modern-normalize/modern-normalize.css';
 import '../scss/style.scss';
+import { initForms } from './services/Form';
 import { setupCounter } from './utils/counter';
 
-setupCounter(document.getElementById('counter'));
+window.addEventListener('DOMContentLoaded', () => {
+  setupCounter(document.getElementById('counter'));
+
+  initForms();
+});
